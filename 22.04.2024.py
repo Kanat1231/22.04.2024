5255e7f92eeddbafd5ce915fa67f28aefa5786b5
Kanat1231 <kanatdauletbaj272@gmail.com>
2024-04-23 11:08:05 +0500

    first commit

---
+++ 22.04.2024.py
@@ -0,0 +1,64 @@
+def mutiple(x, y):
+    return (x * y)
+a = mutiple(4, 4)
+print(a)
+
+
+def mutiple(x, y):
+    print(x * y)
+a = mutiple(4, 8)
+print(a)
+
+def mutiple(x, y):
+    pass
+a = mutiple(4, 8)
+print(a)
+
+def mutiple(x, y):
+    print(x*y)
+    return x+y
+a = mutiple(4, 8)
+print(a)
+
+def mutiple(x, y = 10):
+    print(x*y)
+    return x+y
+a = mutiple(4)
+print(a)
+
+def check_numbers(x, y):
+    if x + y < 100:
+        return "100 ден кышы"
+    else:
+        return "100 ден улкен"
+print(check_numbers(58,50))
+
+
+def finnd_max(list1):
+    return max(list1)
+print(finnd_max([1,2,3,4,5]))
+
+def find_max(list1):
+    my_list_max = list1[0]
+    for i in list1:
+        if i > my_list_max:
+            my_list_max = i
+    return my_list_max
+
+print(find_max([1,2,-1,4,5]))
+
+def find_min(list1):
+    my_list_min = list1[0]
+    for i in list1:
+        if i < my_list_min:
+            my_list_min = i
+    return my_list_min
+
+print(find_min([1,2,1,-2,9]))
+
+
+
+
+
+
+
